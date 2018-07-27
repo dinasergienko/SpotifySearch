f8ff50d1ae0eceabc1633fb0d68772c66ed5eeb6
Dina <sergidka@gmail.com>
2018-07-27 15:04:34 +0300

    changed authorization process to show spotify sign in page

--- client/common/constants.js
+++ client/common/constants.js
@@ -14,7 +14,7 @@ export const AUTH = {
     CLIENT_ID: "92cc66d8a9754a6892b952da2a7eefab",
     CLIENT_SECRET: "9ca5ad4ead0c4b1b860c0b3b190bc247",
     REDIRECT_URL: "http://localhost:8080/callback",
-    SPOTIFY_AUTH_URL: "https://accounts.spotify.com/authorize/?client_id=92cc66d8a9754a6892b952da2a7eefab&response_type=code&redirect_uri=http://localhost:8080/callback&scope=user-read-playback-state,user-modify-playback-state,user-read-private",
+    SPOTIFY_AUTH_URL: "https://accounts.spotify.com/authorize/?client_id=92cc66d8a9754a6892b952da2a7eefab&response_type=code&redirect_uri=http://localhost:8080/callback&scope=user-read-playback-state,user-modify-playback-state,user-read-private&show_dialog=true",
     SPOTIFY_TOKEN_URL: "https://accounts.spotify.com/api/token",
     SPOTIFY_PROFILE_UTL: "https://open.spotify.com/user/",
     ACCESS_TOKEN: "accessToken",
